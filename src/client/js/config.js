a986e23afb7c0f8f2829734dd723cbc69dfbba07
Denis Narushevich <d.narushevich@gmail.com>
2026-09-22 01:57:48 +0300

    increased chunk sizes

--- src/client/js/config.js
+++ src/client/js/config.js
@@ -9,5 +9,5 @@ export default Client.Config = {
     tileSize: 45.255,
     tileZStep: 9.238,
     //chunkSize: (window.matchMedia("handheld").matches ? 8 : 24)
-    chunkSize: (window.matchMedia("(max-width: 800px)").matches ? 16 : 32)
+    chunkSize: (window.matchMedia("(max-width: 800px)").matches ? 12 : 24)
 };
